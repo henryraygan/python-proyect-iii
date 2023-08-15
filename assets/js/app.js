0b3707533f2da8d63d007887ebc168fb136bfdb0
Henry Ramirez <contactoraygan@gmail.com>
2023-08-15 01:24:12 -0600

    adding basic http client

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,32 +8,4 @@ const getFindActor = () => {
     });
   }
 };
-
-const getResults = async () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const q_param = urlParams.get("q");
-  if (q_param) {
-    console.log(q_param);
-    const url = "https://imdb8.p.rapidapi.com/auto-complete?q=" + q_param;
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "86f6096c8emsh78cdec81ef90c92p1ab232jsnafe9ec521eee",
-        "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
-      },
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-};
-
-function getItems() {}
-
 getFindActor();
-getResults();
