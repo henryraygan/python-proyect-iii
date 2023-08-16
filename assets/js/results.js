a6fe01cfa7687474e5779a1c4c29428885ff2492
Henry Ramirez <contactoraygan@gmail.com>
2023-08-15 21:15:05 -0600

    adding page actor

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,14 +1,16 @@
 const getResults = async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const q_param = urlParams.get("q");
+  const movieshtml = document.getElementById("rmovies");
+
   if (q_param) {
-    const movieshtml = document.getElementById("rmovies")
     const data = { q: q_param };
     try {
       const response = await _http.get("/auto-complete", data);
       const { d } = response;
-      for (let i=1; i > d.length; i++) {
-        movieshtml.innerHTML += makeMovie(d[i])
+      for (let i = 1; i < d.length; i++) {
+        console.log(makeMovie(d[i]));
+        movieshtml.innerHTML += makeMovie(d[i]);
       }
       console.log("$http.get =>", d);
     } catch (error) {
@@ -18,12 +20,12 @@ const getResults = async () => {
 };
 getResults();
 
-const makeMovie=(args) => {
-  const {i:imageUrl, l, s} = args;
+const makeMovie = (movie) => {
+  const { i, id, l, s } = movie;
   return `<div class="img-movie__container">
   <div>
     <img
-      src="${imageUrl}"
+      src="${i.imageUrl}"
       alt=""
       class="img-movie__ticket"
     />
@@ -32,8 +34,8 @@ const makeMovie=(args) => {
     <div>
       <h3 class="img-movie__title">${l}</h3>
       <p class="img-movie__text">${s}</p>
-      <a href="#" class="img-movie__link">Streaming Link</a>
+      <a href="actor.html?id=${id}" class="img-movie__link">Información</a>
     </div>
   </div>
-</div>  `;
-}
+</div>`;
+};
