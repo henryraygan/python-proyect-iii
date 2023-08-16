c8304aae9a429083d2e04d4e6de9fe9afb48be1c
Henry Ramirez <contactoraygan@gmail.com>
2023-08-15 20:01:04 -0600

    adding fetch on httpClient

--- assets/js/httpClient.js
+++ assets/js/httpClient.js
@@ -1,35 +1,43 @@
-const $http = (() => {
+const _http = (() => {
   const api = {};
+
   const URL_BASE = "https://imdb8.p.rapidapi.com";
-  const defaultOptions = {
-    headers: {
-      "X-RapidAPI-Key": "86f6096c8emsh78cdec81ef90c92p1ab232jsnafe9ec521eee",
-      "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
-    },
-  };
+  const API_KEY = "86f6096c8emsh78cdec81ef90c92p1ab232jsnafe9ec521eee";
+  const API_HOST = "imdb8.p.rapidapi.com";
+  const defaultHeaders = new Headers({
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": API_HOST,
+  });
 
-  api.request = (url, options, callback) => {
-    const requestOptions = { ...defaultOptions, ...options };
+  api.request = async (url, options, callback) => {
+    const requestOptions = {
+      headers: defaultHeaders,
+      ...options,
+    };
 
-    axios(url, requestOptions)
-      .then((response) => {
-        callback(response.data);
-      })
-      .catch((error) => {
-        console.error("Error on =>", error);
-      });
+    try {
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      callback(data);
+    } catch (error) {
+      console.error("Error on =>", error);
+    }
   };
 
   api.get = (url, data = {}) => {
+    const queryParams = new URLSearchParams(data);
     const options = {
       method: "GET",
-      params: data,
     };
 
     return new Promise((resolve, reject) => {
-      api.request(URL_BASE + url, options, (response) => {
-        resolve(response);
-      });
+      api.request(
+        `${URL_BASE}${url}?${queryParams.toString()}`,
+        options,
+        (response) => {
+          resolve(response);
+        }
+      );
     });
   };
 
@@ -39,7 +47,7 @@ const $http = (() => {
       headers: {
         "Content-Type": "application/json",
       },
-      data: data,
+      body: JSON.stringify(data),
     };
 
     return new Promise((resolve, reject) => {
